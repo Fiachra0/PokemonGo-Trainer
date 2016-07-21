873b11ad237321d88cac6b90b491bc16cfa103df
Fiachra0 <jon.rankin0@gmail.com>
2016-07-21 12:23:21 -0400

    new version

--- trainer.py
+++ trainer.py
@@ -1,21 +1,84 @@
-import requests
+import flask
+import notifier
+from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+from flask_googlemaps import icons
+import os
 import re
+import sys
 import struct
 import json
+import requests
 import argparse
+import getpass
+import threading
+import werkzeug.serving
 import pokemon_pb2
 import time
-import os 
-
 from google.protobuf.internal import encoder
 from google.protobuf.message import DecodeError
-from gpsoauth import perform_master_login, perform_oauth
-
+from s2sphere import *
 from datetime import datetime
 from geopy.geocoders import GoogleV3
+from gpsoauth import perform_master_login, perform_oauth
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.adapters import ConnectionError
+from requests.models import InvalidURL
+from transform import *
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-from s2sphere import *
+
+API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
+LOGIN_URL = \
+    'https://sso.pokemon.com/sso/login?service=https://sso.pokemon.com/sso/oauth2.0/callbackAuthorize'
+LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
+APP = 'com.nianticlabs.pokemongo'
+
+with open('credentials.json') as file:
+    credentials = json.load(file)
+
+PTC_CLIENT_SECRET = credentials.get('ptc_client_secret', None)
+ANDROID_ID = credentials.get('android_id', None)
+SERVICE = credentials.get('service', None)
+CLIENT_SIG = credentials.get('client_sig', None)
+GOOGLEMAPS_KEY = credentials.get('gmaps_key', None)
+
+SESSION = requests.session()
+SESSION.headers.update({'User-Agent': 'Niantic App'})
+SESSION.verify = False
+
+global_password = None
+global_token = None
+access_token = None
+DEBUG = True
+VERBOSE_DEBUG = False  # if you want to write raw request/response to the console
+COORDS_LATITUDE = 0
+COORDS_LONGITUDE = 0
+COORDS_ALTITUDE = 0
+FLOAT_LAT = 0
+FLOAT_LONG = 0
+NEXT_LAT = 0
+NEXT_LONG = 0
+auto_refresh = 0
+default_step = 0.001
+api_endpoint = None
+pokemons = {}
+gyms = {}
+pokestops = {}
+numbertoteam = {  # At least I'm pretty sure that's it. I could be wrong and then I'd be displaying the wrong owner team of gyms.
+    0: 'Gym',
+    1: 'Mystic',
+    2: 'Valor',
+    3: 'Instinct',
+}
+origin_lat, origin_lon = None, None
+is_ampm_clock = False
+
+# stuff for in-background search thread
+
+search_thread = None
 
 def memoize(obj):
     cache = obj.cache = {}
@@ -43,16 +106,21 @@ def time_left(ms):
     (m, s) = divmod(s, 60)
     (h, m) = divmod(m, 60)
     return (h, m, s)
-    
+
+
 def encode(cellid):
     output = []
     encoder._VarintEncoder()(output.append, cellid)
     return ''.join(output)
 
+
 def getNeighbors():
-    origin = CellId.from_lat_lng(LatLng.from_degrees(FLOAT_LAT, FLOAT_LONG)).parent(15)
+    origin = CellId.from_lat_lng(LatLng.from_degrees(FLOAT_LAT,
+                                                     FLOAT_LONG)).parent(15)
     walk = [origin.id()]
+
     # 10 before and 10 after
+
     next = origin.next()
     prev = origin.prev()
     for i in range(10):
@@ -63,63 +131,68 @@ def getNeighbors():
     return walk
 
 
-API_URL = 'https://pgorelease.nianticlabs.com/plfe/rpc'
-LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
-LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
-
-APP = 'com.nianticlabs.pokemongo'
-
-with open('config/credentials.json') as file:
-	credentials = json.load(file)
-
-PTC_CLIENT_SECRET = credentials.get('ptc_client_secret', None)
-ANDROID_ID = credentials.get('android_id', None)
-SERVICE = credentials.get('service', None)
-CLIENT_SIG = credentials.get('client_sig', None)
-GOOGLEMAPS_KEY = credentials.get('gmaps_key', None)
-
-SESSION = requests.session()
-SESSION.headers.update({'User-Agent': 'Niantic App'})
-SESSION.verify = False
-
-global_password = None
-global_token = None
-access_token = None
-DEBUG = True
-COORDS_LATITUDE = 0
-COORDS_LONGITUDE = 0
-COORDS_ALTITUDE = 0
-FLOAT_LAT = 0
-FLOAT_LONG = 0
-
 def f2i(float):
-  return struct.unpack('<Q', struct.pack('<d', float))[0]
+    return struct.unpack('<Q', struct.pack('<d', float))[0]
+
 
 def f2h(float):
-  return hex(struct.unpack('<Q', struct.pack('<d', float))[0])
+    return hex(struct.unpack('<Q', struct.pack('<d', float))[0])
+
 
 def h2f(hex):
-  return struct.unpack('<d', struct.pack('<Q', int(hex,16)))[0]
+    return struct.unpack('<d', struct.pack('<Q', int(hex, 16)))[0]
+
+
+def retrying_set_location(location_name):
+    """
+    Continue trying to get co-ords from Google Location until we have them
+    :param location_name: string to pass to Location API
+    :return: None
+    """
+
+    while True:
+        try:
+            set_location(location_name)
+            return
+        except (GeocoderTimedOut, GeocoderServiceError), e:
+            debug(
+                'retrying_set_location: geocoder exception ({}), retrying'.format(
+                    str(e)))
+        time.sleep(1.25)
+
 
 def set_location(location_name):
     geolocator = GoogleV3()
-    loc = geolocator.geocode(location_name)
+    prog = re.compile('^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$')
+    global origin_lat
+    global origin_lon
+    if prog.match(location_name):
+        local_lat, local_lng = [float(x) for x in location_name.split(",")]
+        alt = 0
+        origin_lat, origin_lon = local_lat, local_lng
+    else:
+        loc = geolocator.geocode(location_name)
+        origin_lat, origin_lon = local_lat, local_lng = loc.latitude, loc.longitude
+        alt = loc.altitude
+        print '[!] Your given location: {}'.format(loc.address.encode('utf-8'))
 
-    print('[!] Your given location: {}'.format(loc.address.encode('utf-8')))
-    print('[!] lat/long/alt: {} {} {}'.format(loc.latitude, loc.longitude, loc.altitude))
-    set_location_coords(loc.latitude, loc.longitude, loc.altitude)
+    print('[!] lat/long/alt: {} {} {}'.format(local_lat, local_lng, alt))
+    set_location_coords(local_lat, local_lng, alt)
+
 
 def set_location_coords(lat, long, alt):
     global COORDS_LATITUDE, COORDS_LONGITUDE, COORDS_ALTITUDE
     global FLOAT_LAT, FLOAT_LONG
     FLOAT_LAT = lat
     FLOAT_LONG = long
-    COORDS_LATITUDE = f2i(lat) # 0x4042bd7c00000000 # f2i(lat)
-    COORDS_LONGITUDE = f2i(long) # 0xc05e8aae40000000 #f2i(long)
+    COORDS_LATITUDE = f2i(lat)  # 0x4042bd7c00000000 # f2i(lat)
+    COORDS_LONGITUDE = f2i(long)  # 0xc05e8aae40000000 #f2i(long)
     COORDS_ALTITUDE = f2i(alt)
+
 
 def get_location_coords():
     return (COORDS_LATITUDE, COORDS_LONGITUDE, COORDS_ALTITUDE)
+
 
 def retrying_api_req(service, api_endpoint, access_token, *args, **kwargs):
     while True:
@@ -175,6 +248,7 @@ def api_req(service, api_endpoint, access_token, *args, **kwargs):
     time.sleep(0.51)
     return p_ret
 
+
 def get_api_endpoint(service, access_token, api=API_URL):
     profile_response = None
     while not profile_response:
@@ -192,9 +266,25 @@ def get_api_endpoint(service, access_token, api=API_URL):
 
     return 'https://%s/rpc' % profile_response.api_url
 
-def get_profile(access_token, api, useauth, *reqq):
-    req = pokemon_pb2.RequestEnvelop()
+def retrying_get_profile(service, access_token, api, useauth, *reqq):
+    profile_response = None
+    while not profile_response:
+        profile_response = get_profile(service, access_token, api, useauth,
+                                       *reqq)
+        if not hasattr(profile_response, 'payload'):
+            debug(
+                'retrying_get_profile: get_profile returned no payload, retrying')
+            profile_response = None
+            continue
+        if not profile_response.payload:
+            debug(
+                'retrying_get_profile: get_profile returned no-len payload, retrying')
+            profile_response = None
 
+    return profile_response
+
+def get_profile(service, access_token, api, useauth, *reqq):
+    req = pokemon_pb2.RequestEnvelop()
     req1 = req.requests.add()
     req1.type = 2
     if len(reqq) >= 1:
@@ -219,25 +309,7 @@ def get_profile(access_token, api, useauth, *reqq):
     req5.type = 5
     if len(reqq) >= 5:
         req5.MergeFrom(reqq[4])
-
-    return api_req(api, access_token, req, useauth = useauth)
-
-def retrying_get_profile(service, access_token, api, useauth, *reqq):
-    profile_response = None
-    while not profile_response:
-        profile_response = get_profile(service, access_token, api, useauth,
-                                       *reqq)
-        if not hasattr(profile_response, 'payload'):
-            debug(
-                'retrying_get_profile: get_profile returned no payload, retrying')
-            profile_response = None
-            continue
-        if not profile_response.payload:
-            debug(
-                'retrying_get_profile: get_profile returned no-len payload, retrying')
-            profile_response = None
-
-    return profile_response
+    return retrying_api_req(service, api, access_token, req, useauth=useauth)
 
 def login_google(username, password):
     print '[!] Google login for: {}'.format(username)
@@ -251,10 +323,24 @@ def login_google(username, password):
     return r2.get('Auth')
 
 def login_ptc(username, password):
-    print('[!] login for: {}'.format(username))
-    head = {'User-Agent': 'niantic'}
+    print '[!] PTC login for: {}'.format(username)
+    head = {'User-Agent': 'Niantic App'}
     r = SESSION.get(LOGIN_URL, headers=head)
-    jdata = json.loads(r.content)
+    if r is None:
+        return render_template('nope.html', fullmap=fullmap)
+
+    try:
+        jdata = json.loads(r.content)
+    except ValueError, e:
+        debug('login_ptc: could not decode JSON from {}'.format(r.content))
+        return None
+
+    # Maximum password length is 15 (sign in page enforces this limit, API does not)
+
+    if len(password) > 15:
+        print '[!] Trimming password to 15 characters'
+        password = password[:15]
+
     data = {
         'lt': jdata['lt'],
         'execution': jdata['execution'],
@@ -267,22 +353,24 @@ def login_ptc(username, password):
     ticket = None
     try:
         ticket = re.sub('.*ticket=', '', r1.history[0].headers['Location'])
-    except e:
+    except Exception, e:
         if DEBUG:
-            print(r1.json()['errors'][0])
+            print r1.json()['errors'][0]
         return None
 
     data1 = {
         'client_id': 'mobile-app_pokemon-go',
         'redirect_uri': 'https://www.nianticlabs.com/pokemongo/error',
-        'client_secret': 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR',
+        'client_secret': PTC_CLIENT_SECRET,
         'grant_type': 'refresh_token',
         'code': ticket,
     }
     r2 = SESSION.post(LOGIN_OAUTH, data=data1)
     access_token = re.sub('&expires.*', '', r2.content)
     access_token = re.sub('.*access_token=', '', access_token)
+
     return access_token
+
 
 def get_heartbeat(service,
                   api_endpoint,
@@ -362,7 +450,7 @@ def get_args():
         "step_limit": 4
     }
     # load config file
-    with open('config/config.json') as data_file:
+    with open('config.json') as data_file:
         data = json.load(data_file)
         for key in data:
             default_args[key] = str(data[key])
@@ -417,113 +505,41 @@ def login(args):
     return api_endpoint, access_token, profile_response
 
 def main():
-    
     full_path = os.path.realpath(__file__)
     (path, filename) = os.path.split(full_path)
 
     args = get_args()
-    
+
+    if args.auth_service not in ['ptc', 'google']:
+        print '[!] Invalid Auth service specified'
+        return
 
     print('[+] Locale is ' + args.locale)
-    pokemons = json.load(
+    pokemonsJSON = json.load(
         open(path + '/locales/pokemon.' + args.locale + '.json'))
 
     if args.debug:
         global DEBUG
         DEBUG = True
-        print('[!] DEBUG mode on')
+        print '[!] DEBUG mode on'
 
-    set_location(args.location)
+    # only get location for first run
+    if not (FLOAT_LAT and FLOAT_LONG):
+      print('[+] Getting initial location')
+      retrying_set_location(args.location)
 
-    access_token = login_google(args.username, args.password)
-    if access_token is None:
-        print('[-] Wrong username/password')
-        return
-    print('[+] RPC Session Token: {} ...'.format(access_token[:25]))
+    if args.auto_refresh:
+        global auto_refresh
+        auto_refresh = int(args.auto_refresh) * 1000
 
-    api_endpoint = get_api_endpoint(args.auth_service, access_token)
-    if api_endpoint is None:
-        raise Exception('[-] RPC server offline')
+    if args.ampm_clock:
+        global is_ampm_clock
+        is_ampm_clock = True
 
-    print '[+] Received API endpoint: {}'.format(api_endpoint)
+    api_endpoint, access_token, profile_response = login(args)
 
-    profile_response = retrying_get_profile(args.auth_service, access_token,
-                                            api_endpoint, None)
-    if profile_response is None or not profile_response.payload:
-        raise Exception('Could not get profile')
-
-    print '[+] Login successful'
-
-    payload = profile_response.payload[0]
-    profile = pokemon_pb2.ResponseEnvelop.ProfilePayload()
-    profile.ParseFromString(payload)
-    print '[+] Username: {}'.format(profile.profile.username)
-
-    creation_time = \
-        datetime.fromtimestamp(int(profile.profile.creation_time)
-                               / 1000)
-    print '[+] You started playing Pokemon Go on: {}'.format(
-        creation_time.strftime('%Y-%m-%d %H:%M:%S'))
-
-    for curr in profile.profile.currency:
-        print '[+] {}: {}'.format(curr.type, curr.amount)
-
-
-    origin = LatLng.from_degrees(FLOAT_LAT, FLOAT_LONG)
-    while True:
-        original_lat = FLOAT_LAT
-        original_long = FLOAT_LONG
-        parent = CellId.from_lat_lng(LatLng.from_degrees(FLOAT_LAT, FLOAT_LONG)).parent(15)
-
-        h = heartbeat(api_endpoint, access_token, response)
-        hs = [h]
-        seen = set([])
-        for child in parent.children():
-            latlng = LatLng.from_point(Cell(child).get_center())
-            set_location_coords(latlng.lat().degrees, latlng.lng().degrees, 0)
-            hs.append(heartbeat(api_endpoint, access_token, response))
-        set_location_coords(original_lat, original_long, 0)
-
-        visible = []
-
-        for hh in hs:
-            for cell in hh.cells:
-                for wild in cell.WildPokemon:
-                    hash = wild.SpawnPointId + ':' + str(wild.pokemon.PokemonId)
-                    if (hash not in seen):
-                        visible.append(wild)
-                        seen.add(hash)
-
-        print('')
-        for cell in h.cells:
-            if cell.NearbyPokemon:
-                other = LatLng.from_point(Cell(CellId(cell.S2CellId)).get_center())
-                diff = other - origin
-                # print(diff)
-                difflat = diff.lat().degrees
-                difflng = diff.lng().degrees
-                direction = (('N' if difflat >= 0 else 'S') if abs(difflat) > 1e-4 else '')  + (('E' if difflng >= 0 else 'W') if abs(difflng) > 1e-4 else '')
-                print("Within one step of %s (%sm %s from you):" % (other, int(origin.get_distance(other).radians * 6366468.241830914), direction))
-                for poke in cell.NearbyPokemon:
-                    print('    (%s) %s' % (poke.PokedexNumber, pokemons[poke.PokedexNumber - 1]['Name']))
-
-        print('')
-        for poke in visible:
-            other = LatLng.from_degrees(poke.Latitude, poke.Longitude)
-            diff = other - origin
-            # print(diff)
-            difflat = diff.lat().degrees
-            difflng = diff.lng().degrees
-            direction = (('N' if difflat >= 0 else 'S') if abs(difflat) > 1e-4 else '')  + (('E' if difflng >= 0 else 'W') if abs(difflng) > 1e-4 else '')
-
-            print("(%s) %s is visible at (%s, %s) for %s seconds (%sm %s from you)" % (poke.pokemon.PokemonId, pokemons[poke.pokemon.PokemonId - 1]['Name'], poke.Latitude, poke.Longitude, poke.TimeTillHiddenMs / 1000, int(origin.get_distance(other).radians * 6366468.241830914), direction))
-
-        print('')
-        walk = getNeighbors()
-        next = LatLng.from_point(Cell(CellId(walk[2])).get_center())
-        if raw_input('The next cell is located at %s. Keep scanning? [Y/n]' % next) in {'n', 'N'}:
-            break
-        set_location_coords(next.lat().degrees, next.lng().degrees, 0)
 
 if __name__ == '__main__':
-    main()
+    args = get_args()
+    register_background_thread(initial_registration=True)
+    app.run(debug=True, threaded=True, host=args.host, port=args.port)
